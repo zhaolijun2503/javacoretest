2753b04da9e3b3059f71c4dbed5253afb28e89e9
zhaolijun <zhaolijun@acfun.cn>
2018-03-12 15:23:05 +0800

    test

--- src/main/java/org/javacore/stream/CollectionStreamTest01.java
+++ src/main/java/org/javacore/stream/CollectionStreamTest01.java
@@ -24,7 +24,7 @@ import java.util.List;
  * <p/>
  * Created by bysocket on 16/7/13.
  */
-public class CollectionStreamTest01 {
+public class CollectionStreamTest01  {
     public static void main(String[] args) {
         List<String> strList = Arrays.asList("a1", "a2", "c3", "c6", "c4");
 
